6a0130198c7ecbadb8ad4b21d4f48e1df622000a
joel224 <joel224@users.noreply.github.com>
2026-05-12 18:56:35 +0530

    upup

--- src/app/bento/page.tsx
+++ src/app/bento/page.tsx
@@ -233,7 +233,7 @@ const Footer = () => {
       <p className="text-center text-zinc-400">
         Made with ❤️ by{" "}
         <a href="#" className="text-red-300 hover:underline">
-          @JoelJoshy
+          @joeljoshy87@gmail.com
         </a>
       </p>
     </footer>
